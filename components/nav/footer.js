1a4ce7595f2df61c8dc52cf0aecd7ba2f2f88004
lamnguyen <nguyenphuonglam96@gmail.com>
2024-09-05 10:36:43 +0700

    update footer bg color

--- components/nav/footer.js
+++ components/nav/footer.js
@@ -2,7 +2,7 @@ import FooterLink from "./footer-link";
 
 export default function Footer() {
     return (
-        <div className="bg-blue-200 flex px-32 py-8 gap-64">
+        <div className="bg-blue-100 flex px-32 py-8 gap-64">
             <div className="flex flex-col gap-4">
                 <FooterLink iconSrc="/x-logo.svg" href="https://x.com/louis_lamnguyen" text="Join me on X / Twitter" />
                 <button className="bg-blue-800 text-slate-200 rounded-md py-2 px-4 w-fit"><a href="https://buymeacoffee.com/louisng" target="_blank" rel="noopener noreferrer">Buy me a coffee</a></button>
